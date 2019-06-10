d62fbd6122d851d793e9742da9323dd4f7b613fc
pankaj040 <51490934+pankaj040@users.noreply.github.com>
2019-06-10 10:53:51 +0530

    Test code pushed to repository.

---
+++ Print the quotient and remainder/Main.java
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int  quotient, remainder;
+    quotient = 365/4;
+    remainder = 365%4;
+
+    printf("Quotient: %d\n", quotient);
+    printf("Remainder: %d", remainder);
+ //Type your code here
+  return 0;
+}
